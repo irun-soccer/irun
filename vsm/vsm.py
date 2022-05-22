3909c917622bbcc389ae2b8dbb29350f42412248
HAN TAE GYUN <kindkiz@naver.com>
2022-05-22 23:27:18 +0900

    feat: add cos sim in vsm

--- vsm/vsm.py
+++ vsm/vsm.py
@@ -1,4 +1,4 @@
-import math
+import numpy as np
 """
 team irun
 vsm model
@@ -8,7 +8,7 @@ docs_name_list = ["0.0-6.5", "6.5-7.0", "7.0-7.5", "7.5-8.0", "8.0-8.5", "8.5-9.
 # 특정 문서 d 에서의 특정 단어 t 의 등장 횟수. docs_tf[d][t] = num
 docs_tf = {}
 # 특정 단어 t 가 등장한 문서의 수. docs_df[t] = num
-# docs_df = {}
+docs_df = {}
 # docs_tf_idf = {}
 
 
@@ -38,7 +38,8 @@ def make_tf_idf_docs():
                     docs_tf[name][word] += 1
                 else:
                     docs_tf[name][word] = 1
-
+                    if word not in docs_df:
+                        docs_df[word] = 0
         # normalize
         for word, tf in docs_tf[name].items():
             docs_tf[name][word] = tf / docs_len
@@ -47,4 +48,42 @@ def make_tf_idf_docs():
     return docs_tf
 
 
-print(make_tf_idf_docs())
+def run(query):
+    if len(docs_tf) == 0:
+        print("please make if-idf docs first.")
+        return
+    query_vector = {}
+    docs_vector = {}
+    cos_sim = {}
+    # init vector
+    for word, value in docs_df.items():
+        query_vector[word] = 0
+        docs_vector[word] = 0
+    for word in query.strip().split(" "):
+        if word in query_vector:
+            query_vector[word] = 1
+    # dict to list
+    query_vector = list(query_vector.values())
+
+    for name in docs_name_list:
+        # init docs vec
+        for word, value in docs_vector.items():
+            docs_vector[word] = 0
+        # update docs vec
+        for word in docs_tf[name]:
+            docs_vector[word] = docs_tf[name][word]
+        # dict to list
+        docs_vector_list = list(docs_vector.values())
+        if np.linalg.norm(query_vector) * np.linalg.norm(docs_vector_list) != 0:
+            cos_sim[name] = np.dot(query_vector, docs_vector_list) / (np.linalg.norm(query_vector) * np.linalg.norm(docs_vector_list))
+        else:
+            cos_sim[name] = 0
+
+    # print cos sim
+    for key, value in cos_sim.items():
+        print(key, ": ", value)
+    return max(cos_sim, key=cos_sim.get)
+
+
+make_tf_idf_docs()
+print("relevant docs:", run("nice"))
