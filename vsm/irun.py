1c1adb2f8adca217a370d99941a0e5489d28c5cc
HAN TAE GYUN <kindkiz@naver.com>
2022-05-22 21:27:28 +0900

    test: delete test files

--- vsm/irun.py
+++
@@ -1,9 +0,0 @@
-import vsm
-
-# docs_tf_idf = vsm.make_tf_idf_docs()
-input_query_vector = {}
-
-# f = open(r'test_input.txt', 'rt', encoding='utf-8')
-docs = vsm.make_tf_idf_docs()
-docs = vsm.run("d")
-print(docs)
