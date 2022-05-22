bb1c472342d50646d6492ff260389a822d7f15c6
tjwjdgus12 <01048733438a@gmail.com>
2022-05-22 20:19:58 +0900

    feat: 20220516-20220522 문서 추출

--- twitter_crawler/doc_maker.py
+++ twitter_crawler/doc_maker.py
@@ -2,10 +2,6 @@ import re
 import json
 import twitter_crawler
 from tensorflow.keras.preprocessing.text import text_to_word_sequence
-#from nltk.corpus import stopwords
-
-with open('twitter_crawler/stopwords.txt', 'r') as f:
-    stopwords = set(f.read().split())
 
 def get_date(datetime):
     return datetime[:10] # YYYY-MM-DD
@@ -19,38 +15,44 @@ def make_doc(match_json_file, corpus_path):
     with open(match_json_file, 'r') as f:
         json_data = json.load(f)
 
-    for match in json_data:
+    for i, match in enumerate(json_data):
         date = get_date(json_data[match]['date'])
+        print(f"\n[Match {i+1} on {date}]")
 
         ratings = dict()
         ratings.update(json_data[match]['player']['home'])
         ratings.update(json_data[match]['player']['away'])
 
         for player, rating in ratings.items():
-            try:
-                replies = twitter_crawler.get_replies(player, date)
+            if rating is None: # bench
+                continue
 
-                rating = float(rating)
-                if rating < 6.0:
-                    interval_document = "0.0-6.0.txt"
-                elif rating < 7.0:
-                    interval_document = "6.0-7.0.txt"
-                elif rating < 7.5:
-                    interval_document = "7.0-7.5.txt"
-                elif rating < 8.0:
-                    interval_document = "7.5-8.0.txt"
-                elif rating < 8.5:
-                    interval_document = "8.0-8.5.txt"
-                else:
-                    interval_document = "8.5-9.9.txt"
+            replies = twitter_crawler.get_replies(player, date)
 
-                print(f"Now crawling {player} on {date}, rating: {rating} -> {interval_document}")
+            rating = float(rating)
+            if rating < 6.5:
+                interval_document = "0.0-6.5.txt"
+            elif rating < 7.0:
+                interval_document = "6.5-7.0.txt"
+            elif rating < 7.5:
+                interval_document = "7.0-7.5.txt"
+            elif rating < 8.0:
+                interval_document = "7.5-8.0.txt"
+            elif rating < 8.5:
+                interval_document = "8.0-8.5.txt"
+            else:
+                interval_document = "8.5-9.9.txt"
 
-                with open(corpus_path + interval_document, 'a', encoding='UTF-8') as f:
-                    for reply in replies:
-                        tokens = pre_process(reply)
-                        f.write(tokens + '\n')
-            except:
-                pass
+            print(f"Now crawling {player}, reply_num: {len(replies)}, rating: {rating} -> {interval_document}")
+            if len(replies) == 0:
+                continue
 
-#make_doc('api_football_requester/result.json', 'corpus/')
+            with open(corpus_path + interval_document, 'a', encoding='UTF-8') as f:
+                for reply in replies:
+                    tokens = pre_process(reply)
+                    if tokens:
+                        for token in tokens:
+                            f.write(token + ' ')
+                        f.write('\n')
+
+make_doc('api_football_requester/result_220515_220522.json', 'corpus/')
--- twitter_crawler/twitter_crawler.py
+++ twitter_crawler/twitter_crawler.py
@@ -1,17 +1,16 @@
 import tweepy
 import twitter_secret as secret
-import datetime
+import datetime, time
 
 consumer_key = secret.twitter_consumer_key
 consumer_secret = secret.twitter_consumer_secret
 access_token = secret.twitter_access_token
 access_secret = secret.twitter_access_secret
 
-def connect_api():
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_secret)
-    api = tweepy.API(auth)
-    return api
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+req_cnt = 0
 
 def next_date(now_date):
     now_date = datetime.date.fromisoformat(now_date)
@@ -19,9 +18,18 @@ def next_date(now_date):
     return next_date.isoformat()
 
 def get_replies(player_name, match_date):
+    global req_cnt
     result = list()
-    api = connect_api()
+    api = tweepy.API(auth, timeout=600)
     query = f'{player_name} filter:replies since:{match_date} until:{next_date(match_date)}'
-    for status in tweepy.Cursor(api.search_tweets, q=query, lang='en', tweet_mode='extended').items():
+    
+    if req_cnt >= 120:
+        print("... almost limit: sleep 1000 sec...")
+        time.sleep(1000)
+        req_cnt = 0
+
+    for status in tweepy.Cursor(api.search_tweets, q=query, lang='en', tweet_mode='extended', count=180).items():
         result.append(status.full_text)
+    req_cnt += 1
+
     return result
