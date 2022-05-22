8f18d28a0a509f841fd1469fe050c91f4ea0f7d2
tjwjdgus12 <01048733438a@gmail.com>
2022-05-22 16:51:49 +0900

    feat: reply text 전처리 및 rating 구간 조정

--- twitter_crawler/doc_maker.py
+++ twitter_crawler/doc_maker.py
@@ -1,9 +1,19 @@
+import re
 import json
 import twitter_crawler
+from tensorflow.keras.preprocessing.text import text_to_word_sequence
+#from nltk.corpus import stopwords
+
+with open('twitter_crawler/stopwords.txt', 'r') as f:
+    stopwords = set(f.read().split())
 
 def get_date(datetime):
     return datetime[:10] # YYYY-MM-DD
 
+def pre_process(text):
+    text = re.sub('@.+', '', text) # remove mention
+    tokens = text_to_word_sequence(text) # tokenize (+ remove stopword)
+    return tokens
 
 def make_doc(match_json_file, corpus_path):
     with open(match_json_file, 'r') as f:
@@ -20,25 +30,27 @@ def make_doc(match_json_file, corpus_path):
             try:
                 replies = twitter_crawler.get_replies(player, date)
 
-                # temporary interval
                 rating = float(rating)
-                if rating < 5.0:
-                    interval_document = "0~5.txt"
-                elif rating < 6.0:
-                    interval_document = "5~6.txt"
+                if rating < 6.0:
+                    interval_document = "0.0-6.0.txt"
                 elif rating < 7.0:
-                    interval_document = "6~7.txt"
+                    interval_document = "6.0-7.0.txt"
+                elif rating < 7.5:
+                    interval_document = "7.0-7.5.txt"
                 elif rating < 8.0:
-                    interval_document = "7~8.txt"
+                    interval_document = "7.5-8.0.txt"
+                elif rating < 8.5:
+                    interval_document = "8.0-8.5.txt"
                 else:
-                    interval_document = "8~10.txt"
+                    interval_document = "8.5-9.9.txt"
 
                 print(f"Now crawling {player} on {date}, rating: {rating} -> {interval_document}")
 
-                with open(corpus_path+interval_document, 'a', encoding='UTF-8') as f:
+                with open(corpus_path + interval_document, 'a', encoding='UTF-8') as f:
                     for reply in replies:
-                        f.write(reply)
+                        tokens = pre_process(reply)
+                        f.write(tokens + '\n')
             except:
                 pass
 
-make_doc('api_football_requester/result.json', 'corpus/')
+#make_doc('api_football_requester/result.json', 'corpus/')
