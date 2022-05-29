85438591774d04dfdc7c9ef5400eb2344b6adffc
HAN TAE GYUN <kindkiz@naver.com>
2022-05-29 13:41:05 +0900

    feat: plot cos sim in vsm

--- vsm/vsm.py
+++ vsm/vsm.py
@@ -1,12 +1,14 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 """
 team irun
 vsm model
 """
-# docs_name_list = ["0.0-6.5", "6.5-7.0", "7.0-7.5", "7.5-8.0", "8.0-8.5", "8.5-9.9"]
-docs_name_list = ["~6.0", "~6.2", "~6.4", "~6.6", "~6.8", "~7.0", "~7.2", "~7.4",
-                  "~7.6", "~7.8", "~8.0", "~8.2", "~8.4", "~8.6", "~8.8", "~9.0",
-                  "~9.4"]
+docs_name_list = ["0.0-6.5", "6.5-7.0", "7.0-7.5", "7.5-8.0", "8.0-8.5", "8.5-9.9"]
+# docs_name_list = ["~6.0", "~6.2", "~6.4", "~6.6", "~6.8", "~7.0", "~7.2", "~7.4",
+#                  "~7.6", "~7.8", "~8.0", "~8.2", "~8.4", "~8.6", "~8.8", "~9.0",
+#                  "~9.4"]
 # 특정 문서 d 에서의 특정 단어 t 의 등장 횟수. docs_tf[d][t] = num
 docs_tf = {}
 # 특정 단어 t 가 등장한 문서의 수. docs_df[t] = num
@@ -19,7 +21,7 @@ def make_tf_idf_docs():
     # 각 문서의 전처리는 전부 되어 있다고 가정함.
     for name in docs_name_list:
         docs_num += 1
-        f = open(r'../corpus2/'+name+'.txt', "rt", encoding='utf-8')
+        f = open(r'../corpus/'+name+'.txt', "rt", encoding='utf-8')
         docs_tf[name] = {}
 
         docs_len = 0
@@ -81,9 +83,27 @@ def run(query):
         else:
             cos_sim[name] = 0
 
+    x, y = [], []
     # print cos sim
     for key, value in cos_sim.items():
         print(key, ": ", value)
+        tmp = key.split("-")
+        if tmp[0] == "0.0":
+            tmp[0] = "5.0"
+        x.append((float(tmp[0]) + float(tmp[1])) / 2)
+        y.append(value)
+
+    # plot
+    plt.figure(figsize=(8, 8))
+    if len(query) < 30:
+        plt.title("cosine similarity, query: " + query)
+    else:
+        plt.title("cosine similarity")
+
+    plt.plot(x, y)
+    plt.tight_layout()
+    plt.show()
+
     return "relevant docs: " + max(cos_sim, key=cos_sim.get)
 
 
