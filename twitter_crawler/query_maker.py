8022a77d8bafe143f48b4c5a3812625f514762b9
tjwjdgus12 <01048733438a@gmail.com>
2022-05-29 13:51:49 +0900

    feat: 테스트 쿼리 추가

--- twitter_crawler/query_maker.py
+++ twitter_crawler/query_maker.py
@@ -1,17 +1,40 @@
 import re
+import json
 import tensorflow.keras.preprocessing.text
 from twitter_crawler import get_replies
 
-with open('vsm/test_input2.txt', 'a',  encoding='UTF-8') as ff:
-    replies = get_replies('Gerson', '2022-05-21')
+def get_player_nameset(match_json_file):
+    names = set()
+    with open(match_json_file, 'r') as f:
+        json_data = json.load(f)
+    for match in json_data:
+        for player_name in json_data[match]['player']['home']:
+            names.update(map(lambda x: x.lower(), player_name.split()))
+        for player_name in json_data[match]['player']['away']:
+            names.update(map(lambda x: x.lower(), player_name.split()))
+    return names
 
+nameset = get_player_nameset('api_football_requester/result_220515_220522.json')
+nameset.union(get_player_nameset('api_football_requester/result_220522_220529.json'))
+
+NAME = 'alaba'
+DATE = '2022-05-29'
+RATING = '7.5'
+
+nameset.update(NAME.split())
+
+with open(f'vsm/test_query/{NAME}_{RATING}.txt', 'a',  encoding='UTF-8') as ff:
+    replies = get_replies(NAME, DATE)
     for text in replies:
+        text = re.sub('[“”‘]', "", text)
         text = re.sub('’', "'", text) # replace single quotation marks
         text = re.sub('(@[A-Za-z0-9]+)|(\w+:\/\/\S+)', '', text) # remove mention, link
         tokens = tensorflow.keras.preprocessing.text.text_to_word_sequence(text) # tokenize
         with open('twitter_crawler/stopwords.txt', 'r') as f:
             stopwords = set(f.read().split())
         tokens = list(filter(lambda x: x not in stopwords, tokens)) # remove stopword
+        tokens = list(filter(lambda x: x not in nameset, tokens)) # remove name
+        tokens = list(filter(lambda x: not x.isdigit(), tokens)) # remove single number
         for token in tokens:
             ff.write(token + ' ')
         ff.write('\n')
