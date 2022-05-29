3c7337c1acad6f8e0bb23f2133295626da242b85
tjwjdgus12 <01048733438a@gmail.com>
2022-05-29 19:59:53 +0900

    feat: 쿼리 테스트 데이터 추가

--- twitter_crawler/query_maker.py
+++ twitter_crawler/query_maker.py
@@ -14,14 +14,9 @@ def get_player_nameset(match_json_file):
             names.update(map(lambda x: x.lower(), player_name.split()))
     return names
 
-nameset = get_player_nameset('api_football_requester/result_220515_220522.json')
-nameset.union(get_player_nameset('api_football_requester/result_220522_220529.json'))
-
-NAME = 'alaba'
+NAME = 'david alaba'
 DATE = '2022-05-29'
 RATING = '7.5'
-
-nameset.update(NAME.split())
 
 with open(f'vsm/test_query/{NAME}_{RATING}.txt', 'a',  encoding='UTF-8') as ff:
     replies = get_replies(NAME, DATE)
@@ -30,11 +25,6 @@ with open(f'vsm/test_query/{NAME}_{RATING}.txt', 'a',  encoding='UTF-8') as ff:
         text = re.sub('’', "'", text) # replace single quotation marks
         text = re.sub('(@[A-Za-z0-9]+)|(\w+:\/\/\S+)', '', text) # remove mention, link
         tokens = tensorflow.keras.preprocessing.text.text_to_word_sequence(text) # tokenize
-        with open('twitter_crawler/stopwords.txt', 'r') as f:
-            stopwords = set(f.read().split())
-        tokens = list(filter(lambda x: x not in stopwords, tokens)) # remove stopword
-        tokens = list(filter(lambda x: x not in nameset, tokens)) # remove name
-        tokens = list(filter(lambda x: not x.isdigit(), tokens)) # remove single number
         for token in tokens:
             ff.write(token + ' ')
         ff.write('\n')
