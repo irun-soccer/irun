fa879a90b3212ae50e6b10f13370ecb8f5d70257
tjwjdgus12 <01048733438a@gmail.com>
2022-05-29 12:10:53 +0900

    feat: 20220522-20220528 문서 업데이트

--- twitter_crawler/doc_maker.py
+++ twitter_crawler/doc_maker.py
@@ -11,23 +11,39 @@ def get_date(datetime):
 
 def pre_process(text):
     text = re.sub('’', "'", text) # replace single quotation marks
-    text = re.sub('(@[A-Za-z0-9]+)|(\w+:\/\/\S+)', '', text) # remove mention, link
+    text = re.sub('(@[A-Za-z0-9_]+)|(\w+:\/\/\S+)', '', text) # remove mention, link
     tokens = text_to_word_sequence(text) # tokenize
     tokens = list(filter(lambda x: x not in stopwords, tokens)) # remove stopword
     return tokens
 
 def get_interval_doc(rating):
-    point = 6.0
-    while point < 10.0:
-        if rating < point:
-            return '~{:.1f}.txt'.format(point)
-        point += 0.2
+    # [0.2 interval]
+    # point = 6.0
+    # while point < 10.0:
+    #     if rating < point:
+    #         return '~{:.1f}.txt'.format(point)
+    #     point += 0.2
+        
+    # [0.5 interval]
+    if rating < 6.5:
+        return "0.0-6.5.txt"
+    elif rating < 7.0:
+        return "6.5-7.0.txt"
+    elif rating < 7.5:
+        return "7.0-7.5.txt"
+    elif rating < 8.0:
+        return "7.5-8.0.txt"
+    elif rating < 8.5:
+        return "8.0-8.5.txt"
+    else:
+        return "8.5-9.9.txt"
 
 def make_doc(match_json_file, corpus_path):
     with open(match_json_file, 'r') as f:
         json_data = json.load(f)
 
     for i, match in enumerate(json_data):
+
         date = get_date(json_data[match]['date'])
         print(f"\n[Match {i+1} on {date}]")
 
@@ -39,25 +55,12 @@ def make_doc(match_json_file, corpus_path):
         ratings.update(json_data[match]['player']['home'])
         ratings.update(json_data[match]['player']['away'])
 
+        flag = True
         for player, rating in ratings.items():
             if rating is None: # bench
                 continue
 
             replies = twitter_crawler.get_replies(player, date)
-
-            # rating = float(rating)
-            # if rating < 6.5:
-            #     interval_document = "0.0-6.5.txt"
-            # elif rating < 7.0:
-            #     interval_document = "6.5-7.0.txt"
-            # elif rating < 7.5:
-            #     interval_document = "7.0-7.5.txt"
-            # elif rating < 8.0:
-            #     interval_document = "7.5-8.0.txt"
-            # elif rating < 8.5:
-            #     interval_document = "8.0-8.5.txt"
-            # else:
-            #     interval_document = "8.5-9.9.txt"
 
             interval_document = get_interval_doc(float(rating))
 
@@ -73,4 +76,4 @@ def make_doc(match_json_file, corpus_path):
                             f.write(token + ' ')
                         f.write('\n')
 
-make_doc('api_football_requester/result_220515_220522.json', 'corpus2/')
+make_doc('api_football_requester/result_220522_220529.json', 'corpus/')
--- twitter_crawler/twitter_crawler.py
+++ twitter_crawler/twitter_crawler.py
@@ -10,7 +10,6 @@ access_secret = secret.twitter_access_secret
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
-req_cnt = 0
 
 def next_date(now_date):
     now_date = datetime.date.fromisoformat(now_date)
@@ -18,18 +17,14 @@ def next_date(now_date):
     return next_date.isoformat()
 
 def get_replies(player_name, match_date):
-    global req_cnt
     result = list()
-    api = tweepy.API(auth, timeout=600)
+    api = tweepy.API(auth, timeout=600, wait_on_rate_limit=True)
     query = f'{player_name} filter:replies since:{match_date} until:{next_date(match_date)}'
     
-    if req_cnt >= 100:
-        print("... almost limit: sleep 1000 sec...")
-        time.sleep(910)
-        req_cnt = 0
-
     for status in tweepy.Cursor(api.search_tweets, q=query, lang='en', tweet_mode='extended', count=180).items():
         result.append(status.full_text)
-    req_cnt += 1
 
     return result
+
+if __name__ == "__main__":
+    print(get_replies('Timothy Castagne', '2022-05-22'))
