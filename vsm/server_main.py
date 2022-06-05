125ab382a66ae179755646efcb77647059658b21
redjen8 <seokwoo2525@gmail.com>
2022-06-05 13:49:46 +0900

    20220605 jsw Feat : POST /api/rating/ vsm 평점 구하는 api 작성

---
+++ vsm/server_main.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/api/rating/', methods=['POST'])
+def post_vsm_rating():
+    result = vsm.run(request.json["query"], request.json["player"])
+    return result
+
+
+if __name__ == '__main__':
+    app.run()
--- vsm/vsm.py
+++ vsm/vsm.py
@@ -116,12 +116,12 @@ def run(query, query_name=""):
         y.append(value)
 
     # plot
-    plt.figure(figsize=(8, 8))
-    plt.title("cosine similarity, query: " + query_name)
+    # plt.figure(figsize=(8, 8))
+    # plt.title("cosine similarity, query: " + query_name)
 
-    plt.plot(x, y)
-    plt.tight_layout()
-    plt.show()
+    # plt.plot(x, y)
+    # plt.tight_layout()
+    # plt.show()
 
     return "relevant docs: " + max(cos_sim, key=cos_sim.get)
 
