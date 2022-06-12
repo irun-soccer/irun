81602233969794ce3f75fb2b419fba1b4f4c8aed
redjen8 <seokwoo2525@gmail.com>
2022-06-12 12:12:21 +0900

    20220612 jsw Feat : json 형식 완성

--- server_main.py
+++ server_main.py
@@ -1,12 +1,14 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from vsm import vsm
 
 app = Flask(__name__)
 
 @app.route('/api/rating/', methods=['POST'])
 def post_vsm_rating():
-    result = vsm.run(request.json["query"], request.json["player"])
-    return result
+    vsm_result = vsm.run(request.json["query"], request.json["player"])
+    response = jsonify({'rating' : vsm_result, 'player' : request.json["player"]})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
 if __name__ == '__main__':
