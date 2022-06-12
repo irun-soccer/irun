21bdaf4ac702908a771fd84cf525799ba9082194
redjen8 <seokwoo2525@gmail.com>
2022-06-12 13:25:52 +0900

    20220612 jsw Feat : api에 변환한 점수 리턴하도록 변경

--- server_main.py
+++ server_main.py
@@ -1,13 +1,14 @@
 from flask import Flask, request, jsonify
-from vsm import vsm
+from vsm import vsm, result_converter
 
 app = Flask(__name__)
 
 @app.route('/api/rating/', methods=['POST'])
 def post_vsm_rating():
     vsm_result = vsm.run(request.json["query"], request.json["player"])
+    estimated_result = result_converter.convert(vsm_result)
     vsm_result = max(vsm_result, key=vsm_result.get)
-    response = jsonify({'rating' : vsm_result, 'player' : request.json["player"]})
+    response = jsonify({'estimated' : estimated_result, 'range' : vsm_result, 'player' : request.json["player"]})
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
