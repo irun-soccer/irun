6c1298ea2041724648f36a1c85982c1370c31909
tjwjdgus12 <01048733438a@gmail.com>
2022-06-12 18:25:59 +0900

    feat: rating convert algorihm update

--- vsm/result_converter.py
+++ vsm/result_converter.py
@@ -1,12 +1,14 @@
 from math import exp
 
-exp_multiple = 50
+exp_multiple = 5
 
 def convert(result):
     if type(result) == dict:
         result = list(result.values())
 
-    normalized_result = list(map(lambda x: exp(x/sum(result)*exp_multiple), result))
+    # normalized_result = list(map(lambda x: exp(x/sum(result)*exp_multiple), result))
+    normalized_result = list(map(lambda x: (x-min(result))/(max(result)-min(result)), result))
+    normalized_result = list(map(lambda x: x*exp_multiple, normalized_result))
     normalized_result = list(map(lambda x: x/sum(normalized_result), normalized_result))
 
     docs_weight = [5, 6.5, 7.5, 8.5, 10]
