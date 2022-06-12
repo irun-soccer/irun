8734db518c84451979bf402101b0fecf8a3ca351
tjwjdgus12 <01048733438a@gmail.com>
2022-06-12 13:15:32 +0900

    feat: result_converter - exp() used

--- vsm/result_converter.py
+++ vsm/result_converter.py
@@ -1,12 +1,14 @@
-max_value_weight = 5
+from math import exp
+
+exp_multiple = 50
 
 def convert(result):
     if type(result) == dict:
         result = list(result.values())
 
-    result[result.index(max(result))] *= max_value_weight
+    normalized_result = list(map(lambda x: exp(x/sum(result)*exp_multiple), result))
+    normalized_result = list(map(lambda x: x/sum(normalized_result), normalized_result))
 
-    normalized_result = list(map(lambda x: x/sum(result), result))
     docs_weight = [5, 6.5, 7.5, 8.5, 10]
 
     rating = sum([i*j for i, j in zip(normalized_result, docs_weight)])
